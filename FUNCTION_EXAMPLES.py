e3e3b4f5c8e858f8c3afc26b28b3dfff2c21b9bf
browni1608 <IDBmarch28@gmail.com>
2024-11-11 15:54:33 +0000

    created file

---
+++ FUNCTION_EXAMPLES.py
@@ -0,0 +1,21 @@
+# Learning to use user-defined functions
+
+# Define a add function 
+def add(num1, num2, num3):
+    product = num1 + num2 + num3
+    print(product)
+# Define a multiply function 
+def multiply(num1, num2, num3):
+    product = num1 * num2 * num3
+    print(product)
+
+# DEfine main function - all your logic goes here
+def main():
+    first_num = int(input("Gimmie a  numer"))
+    second_num = int(input("Gimmie a number"))
+    third_num = int(input("Gimmie a number"))
+    # Call the multiply fnction 
+    multiply(2,7,2)
+
+# Call main function 
+if _name_ == "_main_"
