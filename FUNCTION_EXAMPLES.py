569f295a9e2ea20c58f986df1ec7d513fa19bb09
browni1608 <IDBmarch28@gmail.com>
2024-11-11 16:35:27 +0000

    changes

--- FUNCTION_EXAMPLES.py
+++ FUNCTION_EXAMPLES.py
@@ -4,18 +4,36 @@
 def add(num1, num2, num3):
     product = num1 + num2 + num3
     print(product)
+
 # Define a multiply function 
 def multiply(num1, num2, num3):
     product = num1 * num2 * num3
     print(product)
-
+def returnNum():
+    userInput = input("Give me a big number: ")
+    while not userInput.isn99umeric():
+        print("value mut be a number")
+def getName(lastName):
+    name = input("Enter your first name")
+    fullname = "*******"+ name + "**********" + lastName
+    return fullname
 # DEfine main function - all your logic goes here
 def main():
     first_num = int(input("Gimmie a  numer"))
     second_num = int(input("Gimmie a number"))
     third_num = int(input("Gimmie a number"))
     # Call the multiply fnction 
-    multiply(2,7,2)
+    multiply(first_num,second_num,third_num)
 
+    # Call the add function 
+    add(first_num, second_num, third_num)
+# Call value returning fnc
+bigNum = returnNum()
+
+print(bigNum * 5)
+
+print(getName("Brown"))
 # Call main function 
-if _name_ == "_main_"
+if __name__ == "_main_":
+    main()
+e
